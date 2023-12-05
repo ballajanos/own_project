905eb0b1c186965a582e75f00420eef62ff253a9
janos <janos.balla@emanuel.ro>
2023-12-05 02:04:17 +0200

    modify color tab

--- lib/widgets/color_tab.dart
+++ lib/widgets/color_tab.dart
@@ -2,25 +2,38 @@ import 'package:flutter/material.dart';
 
 class ColorTab extends StatelessWidget {
   const ColorTab({
-    super.key,
+    Key? key,
     required this.color,
-    this.width = 60,
-    this.height = 60,
-  });
+    this.width = 80,
+    this.height = 80,
+    this.icon,
+    this.child,
+  }) : super(key: key);
 
   final Color color;
-
   final double? width;
-
   final double? height;
+  final Icon? icon;
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
     return Tab(
-      child: SizedBox(
+      child: Container(
         width: width,
         height: height,
-        child: ColoredBox(color: color),
+        decoration: BoxDecoration(
+          color: color,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (icon != null) icon!,
+            if (child != null)
+              SizedBox(width: 4), // Add space between icon and child
+            if (child != null) child!,
+          ],
+        ),
       ),
     );
   }
